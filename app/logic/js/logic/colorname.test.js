4c16a8a043c498560661cbb9e6d0297754a7dd33
Dmitry Kisler <admin@dkisler.com>
2022-08-21 13:09:42 +0200

    Fix go colorname sqrt logic (#1)
    * Fix distance calculation to get color name
    
    Signed-off-by: Dmitry Kisler <admin@dkisler.com>
    
    * Add init js logic
    
    Signed-off-by: Dmitry Kisler <admin@dkisler.com>
    
    * Add js test and build scripts
    
    Signed-off-by: Dmitry Kisler <admin@dkisler.com>
    
    Signed-off-by: Dmitry Kisler <admin@dkisler.com>

---
+++ app/logic/js/logic/colorname.test.js
@@ -0,0 +1,24 @@
+const tests = [
+    {
+        name: "happy path: Aero",
+        in: {r: 124, g: 185, b: 232},
+        want: "Aero",
+    },
+    {
+        name: "happy path: Black",
+        in: {r: 0, g: 0, b: 0},
+        want: "Black",
+    },
+    {
+        name: "happy path: White",
+        in: {r: 255, g: 255, b: 255},
+        want: "White",
+    },
+]
+
+tests.forEach((test) => {
+    const got = name(test.in.r, test.in.g, test.in.b);
+    if (got !== test.want) {
+        throw Error(`Test "${test.name}". got: ${got}, want: ${test.want}`);
+    }
+});
